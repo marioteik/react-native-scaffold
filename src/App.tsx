e6bb98d37d5b3f7dbc8550c6c4734db8e448ec7a
Mario Teik <mario.teik@gmail.com>
2023-04-18 14:44:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = 'bg-neutral-300 dark:bg-slate-900';
+
+  return (
+    <SafeAreaView className={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        className={backgroundStyle}>
+        <View className="bg-white dark:bg-black">
+          <Text>Test</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
